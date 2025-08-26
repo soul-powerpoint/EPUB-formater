9e603b8e0dc4f220be880649f8306c9556fa0c52
XIANZU <0520hyx@gmail.com>
2025-08-26 20:44:28 +1000

    implement

--- src/formater.c
+++ src/formater.c
@@ -4,8 +4,18 @@
 #include <zip.h>
 #include <libxml/parser.h>
 
-static void format_epub(char *path) {
+static char *zip_read(zip_t *zip_file, const char *path, zip_uint64_t *size) {
+    struct zip_stat zip_st;
 
+    zip_stat_init(&zip_st);
+}
+
+static void format_epub(char *path) {
+    int err;
+    zip_t *zip_file = zip_open(path, 0, &err);
+
+    zip_uint64_t container_size;
+    char *container = zip_read(zip_file, "META-INF/container.xml");
 }
 
 int main (int argc, char **argv) {
@@ -18,3 +28,4 @@ int main (int argc, char **argv) {
 
     return 0;
 }
+

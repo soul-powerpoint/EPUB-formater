c2dca5f6398dfb5c74c4649d4c22372e10955e1c
XIANZU <0520hyx@gmail.com>
2025-09-04 23:16:05 +0900

    zip_helper

--- src/zip_helper.c
+++ src/zip_helper.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <zip.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
 
-static char *zip_read(zip_t *zip_file_archive, char *path, zip_uint64_t *size) {
+char *zip_read(zip_t *zip_file_archive, char *path, zip_uint64_t *size) {
     struct zip_stat zip_st;
     zip_stat_init(&zip_st);
     zip_stat(zip_file_archive, path, 0, &zip_st);
@@ -14,4 +16,18 @@ static char *zip_read(zip_t *zip_file_archive, char *path, zip_uint64_t *size) {
 
     *size = zip_st.size;
     return buf;
+}
+
+char *get_opf_str(zip_t *zip_archive, char **full_path, zip_uint64_t *container_size, zip_uint64_t *opf_size) {
+    char *container = zip_read(zip_archive, "META-INF/container.xml", container_size);
+    xmlChar *full_path_src = container_parse(container);
+    char *opf_str = zip_read(zip_archive, (char *) full_path_src, opf_size);
+    printf("full_path_src = \n%s\n\n", full_path_src);
+
+    *full_path = (char *) malloc(sizeof(char) * (strlen(full_path_src)) + 1);
+
+    strcpy(*full_path, full_path_src);
+    xmlFree(full_path_src);
+    free(container);
+    return opf_str;
 }
--- src/zip_helper.h
+++ src/zip_helper.h
@@ -2,7 +2,12 @@
 #define ZIP_HELPER
 
 #include <stdio.h>
+#include <zip.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
 
-void language_change(char *xml_str);
+char *zip_read(zip_t *zip_file_archive, char *path, zip_uint64_t *size);
+
+char *get_opf_str(zip_t *zip_archive, char **full_path, zip_uint64_t *container_size, zip_uint64_t *opf_size);
 
 #endif

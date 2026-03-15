76a4fb2c7046952f8af9a7a002ecc5b63d8c0ff6
XIANZU <0520hyx@gmail.com>
2026-03-16 02:22:18 +0800

    okokok

--- src/formator.py
+++ src/formator.py
@@ -8,6 +8,18 @@ class EpubFormator:
         self.opf_path  = get_opf_path(epub_path)
         self.data      = parse_opf(epub_path, self.opf_path)
 
+    def get_title(self):
+        return self.data.title
+    
+
+    def get_author(self):
+        return self.data.author
+    
+    
+    def get_language(self):
+        return self.data.language
+
+
     def set_language(self, language: str) -> None:
         set_language(self.data, language)
         save_opf(self.epub_path, self.opf_path, self.data)

37bc972e36483dad4547fbfcda4227d15db24673
XIANZU <0520hyx@gmail.com>
2026-03-17 00:05:56 +0800

    cover

--- src/formator.py
+++ src/formator.py
@@ -1,6 +1,6 @@
 from epub_io.container import get_opf_path
-from epub_io.opf import parse_opf, save_opf
-from operations.metadata import set_language
+from epub_io.opf import parse_opf, save_opf, get_cover_item
+from operations.metadata import set_language, set_cover
 
 class EpubFormator:
     def __init__(self, epub_path: str):
@@ -20,6 +20,19 @@ class EpubFormator:
         return self.data.language
 
 
+    def get_cover(self):
+        return self.data.cover_meta_id
+
+
     def set_language(self, language: str) -> None:
         set_language(self.data, language)
+        save_opf(self.epub_path, self.opf_path, self.data)
+
+
+    def set_cover(self, cover: str) ->None:
+        cover_item = get_cover_item(self.data)
+        if cover_item is None:
+            raise ValueError("No cover image found in this EPUB")
+
+        set_cover(self.epub_path, cover, cover_item)
         save_opf(self.epub_path, self.opf_path, self.data)
